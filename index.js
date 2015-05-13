173d1c99574598568a6d86e498be24ef5624fc1e
Erik Donohoo <erikdonohoo@gmail.com>
2015-05-12 18:40:02 -0600

    make trees with gain

--- index.js
+++ index.js
@@ -21,13 +21,14 @@ function Tree (arffData, className, useAccuracy, crossValidateNum) {
 
 function createTree (arffData, className, featureList) {
 
-	var instances = _.uniq(_.pluck(arffData, className));
+	var classes = _.uniq(_.pluck(arffData, className));
 
-	// Check recursion base cases
-	if (featureList.length === 1) {
-		return {type: 'result', val: instances[0], name: instances[0]};
+	// Check if only one instance remains
+	if (classes.length === 1) {
+		return {type: 'result', val: aData.types[className].oneof[classes[0]]};
 	}
 
+	// Check if no features left to compare on
 	if (featureList.length === 0) {
 		var topAttr = _.sortBy(instances, function (instance) {
 			return instances.filter(function (i) {
@@ -47,53 +48,114 @@ function createTree (arffData, className, featureList) {
 		bestAttribute = findMaxGain(arffData, className, featureList);
 	}
 
-	var remainingAttributes = _.without(featureList, bestAttribute);
+	var nominal = aData.types[bestAttribute].type === 'nominal';
+	var remainingAttributes = nominal ? _.without(featureList, bestAttribute) : featureList;
 	var possibleValues = _.uniq(_.pluck(arffData, bestAttribute));
+	var splitPoint;
 
-	// if (arffData.types[bestAttribute].type === 'nominal') {
-	// 	possibleValues.map(function (value) {
-	// 		return arffData.types[bestAttribute].oneof[value];
-	// 	});
-	// }
+	if (nominal) {
+		possibleValues = aData.types[bestAttribute].oneof;
+	} else {
+		splitPoint = bestSplit(arffData, bestAttribute, _.pluck(arffData, bestAttribute), entropy(_.pluck(arffData, className)), className);
+	}
 
 	var treeNode = {name: bestAttribute, type: 'attribute'};
-	treeNode.vals = possibleValues.map(function (value) {
-		var newDataSet = arffData.filter(function (instance) {
-			instance[bestAttribute] = value;
+	if (nominal) {
+		treeNode.vals = possibleValues.map(function (value, index) {
+			var newDataSet = arffData.filter(function (instance) {
+				return aData.types[bestAttribute].type === 'nominal' ?
+					aData.types[bestAttribute].oneof[instance[bestAttribute]] === value :
+					instance[bestAttribute] === value;
+			});
+			var childNode = {name: value, type: 'attribute_value'};
+			childNode.child = createTree(newDataSet, className, remainingAttributes);
+			return childNode;
 		});
-		var childNode = {name: value, type: 'attribute_value'};
-		childNode.child = createTree(newDataSet, className, remainingAttributes);
-	});
+	} else {
+		// Numeric split
+		var firstHalf = arffData.filter(function (item) {
+			return item[bestAttribute] <= splitPoint;
+		});
+		var secondHalf = arffData.filter(function (item) {
+			return item[bestAttribute] > splitPoint;
+		});
+		treeNode.vals = [];
+		var childNode = {name: '<= ' + splitPoint, type: 'attribute-value'};
+		childNode.child = createTree(firstHalf, className, remainingAttributes);
+		var childNode2 = {name: '> ' + splitPoint, type: 'attribute-value'};
+		childNode2.child = createTree(secondHalf, className, remainingAttributes);
+		treeNode.vals.push(childNode);
+		treeNode.vals.push(childNode2);
+	}
 
 	return treeNode;
 }
 
 function findMaxGain (data, className, featureList) {
+
+	var entropyOfSet = entropy(_.pluck(data, className));
 	return _.max(featureList, function (feature) {
 
-		var featureValues = aData.types[feature].type === 'nominal' ?
+		var nominal = aData.types[feature].type === 'nominal';
+		var featureValues = nominal ?
 			aData.types[feature].oneof :
-			_.unique(_.pluck(data, feature));
-
-		var entropyOfSet = entropy(_.pluck(data, className));
+			_.pluck(data, feature);
 
 		var setSize = data.length;
-		var entropies = featureValues.map(function (featureVal) {
-			var subset = data.filter(function (instance) {
-				var val = aData.types[feature].type === 'nominal' ?
-					aData.types[feature].oneof[instance[featureVal]] :
-					instance[featureVal];
-				return val === featureVal;
+		var entropies;
+		if (nominal) {
+			// Nominal entropy
+			entropies = featureValues.map(function (featureVal) {
+				// Find all data with this value
+				var subset = data.filter(function (instance) {
+					var val = aData.types[feature].type === 'nominal' ?
+						aData.types[feature].oneof[instance[feature]] :
+						instance[featureVal];
+					return val === featureVal;
+				});
+				return (subset.length / setSize) * entropy(_.pluck(subset, className));
 			});
-			return (subset.length / setSize) * entropy(_.pluck(subset, className));
-		});
+		} else {
+			// Numeric entropy
+			featureValues.sort();
+
+			var splitPoint = bestSplit(data, feature, featureValues, entropyOfSet, className);
+			var firstHalf = data.filter(function (item) {
+				return item[feature] <= splitPoint;
+			});
+			var secondHalf = data.filter(function (item) {
+				return item[feature] > splitPoint;
+			});
+			entropies = [firstHalf, secondHalf].map(function (list) {
+				return (list.length / data.length) * entropy(_.pluck(list, className));
+			});
+		}
 
 		var sumOfEntropies =  entropies.reduce(function (a,b) {
 			return a + b;
 		}, 0);
 
-		console.log(feature, entropyOfSet - sumOfEntropies);
 		return entropyOfSet - sumOfEntropies;
+	});
+}
+
+function bestSplit(data, feature, featureValues, setEntropy, className) {
+	var unique = _.uniq(featureValues);
+	return _.max(unique, function (number) {
+		var firstHalf = data.filter(function (item) {
+			return item[feature] <= number;
+		});
+		var secondHalf = data.filter(function (item) {
+			return item[feature] > number;
+		});
+
+		var entropies = [firstHalf, secondHalf].map(function (list) {
+			return (list.length / data.length) * entropy(_.pluck(list, className));
+		});
+
+		return setEntropy - entropies.reduce(function (a, b) {
+			return a + b;
+		}, 0);
 	});
 }
 
@@ -118,7 +180,17 @@ function ratio(value, list) {
 	return totalValues / list.length;
 }
 
-arff.load('lenses.arff', function (err, data) {
-	var tree = new Tree(data, 'contact-lenses', args.accuracy, args.cv);
-	console.log(tree.root);
+function printTree(tree) {
+	console.log(tree);
+	if (tree.vals) {
+		tree.vals.forEach(function (branch) {
+			console.log('\n\nComes from ' + tree.name + ' ' + tree.type + ' ' + branch.name);
+			printTree(branch.child);
+		});
+	}
+}
+
+arff.load('iris.arff', function (err, data) {
+	var tree = new Tree(data, 'class', args.accuracy, args.cv);
+	printTree(tree.root);
 });
